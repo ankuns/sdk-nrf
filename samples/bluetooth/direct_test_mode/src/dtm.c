c4d797f1b1e397568f8522cf7ada22fe70716218
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2024-11-06 11:47:25 +0100

    [fork] dtm: dtm_fem_tx_power_control_set only with CONFIG_FEM
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

--- samples/bluetooth/direct_test_mode/src/dtm.c
+++ samples/bluetooth/direct_test_mode/src/dtm.c
@@ -2174,12 +2174,14 @@ int dtm_test_receive(uint8_t channel)
 	return 0;
 }
 
+#if CONFIG_FEM
 int dtm_fem_tx_power_control_set(uint32_t tx_power_control)
 {
 	dtm_inst.fem.tx_power_control = tx_power_control;
 	
 	return 0;
 }
+#endif
 
 int dtm_test_transmit(uint8_t channel, uint8_t length, enum dtm_packet pkt)
 {
