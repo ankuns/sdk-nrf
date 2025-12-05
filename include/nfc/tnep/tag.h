f314880c8d72743878e0ff517e3f1cf3551314bb
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2025-12-05 10:06:48 +0100

    nfc: separate TNEP signalling functionality for NFC tag
    The NFC TNEP signalling functionality in the nRF Connect SDK
    is based on Zephyr kernel primitives.  In the Bare Metal the Zephyr
    primitives are not available. The Bare Metal will need a separate
    implementation based on Bare Metal primitives. To allow that the
    TNEP signalling is extracted from the subsys/nfc/tnep/tag.c into
    separate files.
    
    The include/nfc/tnep/tag_signalling.h is the header that defines
    the signalling API that is used by subsys/nfc/tnep/tag.c
    implementation.
    
    The subsys/nfc/tnep/tag.c is now signalling-agnostic and uses the
    signalling API defined in include/nfc/tnep/tag_signalling.h.
    
    The subsys/nfc/tnep/tag_signalling_zephyr.c contains the Zephyr-based
    implementation of the signalling API and the Zephyr-specific
    initialization function.
    
    Because the way of handling signalling is platform-specific,
    the `nfc_tnep_tag_init()` API is changed to not require
    `struct k_poll_event *events, uint8_t event_cnt` parameters.
    Instead the platform-specific signalling initialization must be called
    before calling `nfc_tnep_tag_init()`. The Zephyr-based signalling
    initialization function is named `nfc_tnep_tag_signalling_init()`,
    its prototype is declared in include/nfc/tnep/tag_signalling_zephyr.h,
    and it is implemented in subsys/nfc/tnep/tag_signalling_zephyr.c.
    
    To allow selection of applicable signalling implementation the
    Kconfig choice `NFC_TNEP_TAG_SIGNALLING` is added. In the
    nRF Connect SDK it has only one choice:
    `NFC_TNEP_TAG_SIGNALLING_ZEPHYR`. The Bare Metal will extend the
    choice and provide separate signalling implementation.
    
    The applications based on nRF Connect SDK using the NFC TNEP tag
    functionality must be updated in the following way:
    - include the signalling header
      `#include <nfc/tnep/tag_signalling_zephyr.h>`
    - call the signalling initialization function
      `nfc_tnep_tag_signalling_init()` before calling `nfc_tnep_tag_init()`
      The `events` and `event_cnt` parameters that were previously passed
      to `nfc_tnep_tag_init()` now are to be passed to
      `nfc_tnep_tag_signalling_init` function.
    
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

--- include/nfc/tnep/tag.h
+++ include/nfc/tnep/tag.h
@@ -17,9 +17,6 @@
 #include <nfc/tnep/base.h>
 #include <zephyr/kernel.h>
 
-/** NFC TNEP library event count. */
-#define NFC_TNEP_EVENTS_NUMBER 2
-
 /** Maximum Service Waiting Time. */
 #define NFC_TNEP_TAG_MAX_WAIT_TIME 63
 
@@ -199,8 +196,7 @@ int nfc_tnep_tag_tx_msg_buffer_register(uint8_t *tx_buff,
  * @retval 0 If the operation was successful.
  *           Otherwise, a (negative) error code is returned.
  */
-int nfc_tnep_tag_init(struct k_poll_event *events, uint8_t event_cnt,
-		      nfc_payload_set_t payload_set);
+int nfc_tnep_tag_init(nfc_payload_set_t payload_set);
 
 /**
  * @brief Create the Initial TNEP NDEF message.
